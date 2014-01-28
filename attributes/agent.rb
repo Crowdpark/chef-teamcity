9a2bec7e697be97c9b743e0fb41298be65f5d4da
Wouter Westenbrink <wouter@crowdpark.com>
2014-01-28 10:27:05 +0100

    disable default agent

--- attributes/agent.rb
+++ attributes/agent.rb
@@ -15,7 +15,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-node.default['teamcity']['agents']['default'] = {}
-
-Teamcity::Agent.new('default', node).set_defaults
